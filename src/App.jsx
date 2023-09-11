5c95e776f60d4de078bb47b4c528456f819ed316
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-09-12 00:25:11 +0300

    finished header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">   
+      <Header />
+    </div>
+  );
 }
 
 export default App;
