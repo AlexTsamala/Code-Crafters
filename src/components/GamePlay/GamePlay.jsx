8bc0a5425cd9f25d8202e1c7da8485c66e4bfcde
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-09-12 20:44:25 +0300

    create folder Menu for opacity

--- src/components/GamePlay/GamePlay.jsx
+++ src/components/GamePlay/GamePlay.jsx
@@ -1,22 +1,32 @@
+// import starterPage from '../../starterPage/starterPage';
 
 // import Button from '../shared/button/button';
 import './GamePlay.css';
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Game from './game/Game';
+import Menu from './Menu/Menu';
+import { useState } from 'react';
+
 
 export default function GamePlay() {
-
+    const [menuButtonActive, setMenuButtonActive] = useState(false);
+    const menuButtonHandler = () => {
+        setMenuButtonActive(!menuButtonActive);
+    }
+    
+  
 
     return (
 
-
-        <div className="gamePlay">
-           <Header />
-           <Game />
-           <Footer />
-
-
-        </div>
+            <div className="gamePlay">
+                <div className={`${menuButtonActive? 'opacity50': 'HeaderGameFooter' }`}>
+                    <Header name={menuButtonHandler} />
+                    <Game />
+                    <Footer />
+                </div>
+                <Menu setMenuButtonActive={setMenuButtonActive} menuButtonActive={menuButtonActive} menuButtonHandler={menuButtonHandler} />
+               
+            </div>
     )
 }
