52208350541afe515358e83ab7cffdcfdfb1fca9
salomechik <salome.chikvaidze.05@gmail.com>
2023-09-12 22:23:20 -0700

    update component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage"
+import ResultPage from "./components/ResultPage/ResultPage";
 import { useState } from "react"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return <>
   <HomePage/>
   startGameHandler={startGameHandler}
+  <ResultPage/>
     </>;
 }
 
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -16,26 +16,26 @@ const HomePage = () => {
             <div className="first">
                 <h2 className="select-theme">Select Theme</h2>
                 <div className="buttons">
-                <StarterPageButton description={"Numbers"} backgroundColor={selectedTheme === "Numbers" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedTheme} />
-                <StarterPageButton description={"Icons"} backgroundColor={selectedTheme === "Icons" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedTheme}  />
+                <StarterPageButton description={"Numbers"} backgroundColor={selectedTheme === "Numbers" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedTheme} key={1}/>
+                <StarterPageButton description={"Icons"} backgroundColor={selectedTheme === "Icons" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedTheme} key={2} />
                 </div>
                 
             </div>
             <div className="second">
                 <h2 className="numbers-of-players">Numbers of Players</h2>
                 <div className="row">
-                <StarterPageButton description={"1"}  backgroundColor={selectedPlayers === "1" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers"/>
-                <StarterPageButton description={"2"}  backgroundColor={selectedPlayers === "2" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers"/>
-                <StarterPageButton description={"3"}  backgroundColor={selectedPlayers === "3" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers"/>
-                <StarterPageButton description={"4"}  backgroundColor={selectedPlayers === "4" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers"/>
+                <StarterPageButton description={"1"}  backgroundColor={selectedPlayers === "1" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers" />
+                <StarterPageButton description={"2"}  backgroundColor={selectedPlayers === "2" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers" />
+                <StarterPageButton description={"3"}  backgroundColor={selectedPlayers === "3" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers" />
+                <StarterPageButton description={"4"}  backgroundColor={selectedPlayers === "4" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedPlayers} className="numbers" />
                 </div>
                 
             </div>
             <div className="third">
                 <h2 className="grid-size">Grid Size</h2>
                 <div className="buttons row">
-                <StarterPageButton description={"4x4"} backgroundColor={selectedGridSize === "4x4" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedGridSize}/>
-                <StarterPageButton description={"6x6"} backgroundColor={selectedGridSize === "6x6" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedGridSize}/>
+                <StarterPageButton description={"4x4"} backgroundColor={selectedGridSize === "4x4" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedGridSize} key={7}/>
+                <StarterPageButton description={"6x6"} backgroundColor={selectedGridSize === "6x6" ? "#304859" : "#BCCED9"} setChosenButton={setSelectedGridSize} key={8}/>
                 </div>
             </div>
             <button className="yellow-button">Start Game</button>
