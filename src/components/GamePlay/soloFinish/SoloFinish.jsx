cf2b71dc5ca6f2d907b3a8cfea5ffe98318ec83d
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-09-13 22:25:31 +0300

    change position of soloFinish

--- src/components/GamePlay/soloFinish/SoloFinish.jsx
+++ src/components/GamePlay/soloFinish/SoloFinish.jsx
@@ -6,26 +6,26 @@ import './SoloFinish.css';
 export default function SoloFinish({seconds, minutes}) {
 
     return (
-        <div className="soloFinish">
-            <span className='fontFamily youDidItText'>You did it!</span>
-            <span className='fontFamily gameOverSoloText'>Game over! Here’s how you got on…</span>
-            <div className='timeAndMoveBox'>
-                <span className='fontFamily timeElapsed'>Time Elapsed</span>
-                <span className='fontFamily showTimeAndMove'>
-                {`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}
-                </span>
+        <div className='soloFinishBigBox'>
+            <div className="soloFinish">
+                <span className='fontFamily youDidItText'>You did it!</span>
+                <span className='fontFamily gameOverSoloText'>Game over! Here`s how you got on…</span>
+                <div className='timeAndMoveBox'>
+                    <span className='fontFamily timeElapsed'>Time Elapsed</span>
+                    <span className='fontFamily showTimeAndMove'>
+                    {`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}
+                    </span>
+                </div>
+                <div className='timeAndMoveBox marginAdd'>
+                    <span className='fontFamily timeElapsed'>Moves Taken</span>
+                    <span className='fontFamily showTimeAndMove'>
+                        {/* {moves with props } */} Moves
+                    </span>
+                </div>
+                <button className='fontFamily soloRestart'>Restart</button>
+                <button className='fontFamily soloSetup marginAdd15'>Setup New Game</button>
             </div>
-            <div className='timeAndMoveBox marginAdd'>
-                <span className='fontFamily timeElapsed'>Moves Taken</span>
-                <span className='fontFamily showTimeAndMove'>
-                    {/* {moves with props } */} Moves
-                </span>
-            </div>
-            <button className='fontFamily soloRestart'>Restart</button>
-            <button className='fontFamily soloSetup marginAdd15'>Setup New Game</button>
-
-
-
         </div>
+          
     )
 }
