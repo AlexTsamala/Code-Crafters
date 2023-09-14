03a2e7547b1e3fcec6a690f99b216141b22dde6a
Cotne2003 <basiashvilic@gmail.com>
2023-09-15 00:26:34 +0400

    added full game

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
     <div className="App">
       {/* eslint-disable-next-line react/jsx-no-undef */}
       {/* აქ, მე–10 ხაზზე, დატოვეთ კომენტარად (გიორგი) */}
-      <StarterPage
-      // startGameHandler={startGameHandler}
-      // სალომე, მე–18, მე–4, მე–9 ხაზზე რაც წერია ამოაკომენტარე. props-ად მიგაქვს, შენს კოდში
-      // და start Games-ის ღილაკს რომ დააწვები, გააქრობს შენს გვერდს და გაეშვება GamePlay-ს კოდი (გიორგი)
-      />
+
+      {/* startGameHandler={startGameHandler}
+      სალომე, მე–18, მე–4, მე–9 ხაზზე რაც წერია ამოაკომენტარე. props-ად მიგაქვს, შენს კოდში
+      და start Games-ის ღილაკს რომ დააწვები, გააქრობს შენს გვერდს და გაეშვება GamePlay-ს კოდი (გიორგი) */}
+      {/* <StarterPage /> */}
       <GamePlay />
     </div>
   );
