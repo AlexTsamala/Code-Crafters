58091c4bb891036e30f29ff1ee49fe3be6bffcf3
Cotne2003 <basiashvilic@gmail.com>
2023-09-15 07:10:32 +0400

    removed bugs and errors

--- src/components/GamePlay/game/Game.jsx
+++ src/components/GamePlay/game/Game.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import NumberButton from "./NumberButton";
-import IconsButton from "./IconsButton";
 import "./Game.css";
 import { numbers, numbersMini } from "./Number";
 import { icons, iconsMini } from "./Icons";
+import IconsButton from "./IconsButton";
 
 function Game({
   stopGamePlaysHandler,
@@ -12,12 +12,16 @@ function Game({
 }) {
   const [itemsIsMini, setItemsIsMini] = useState(true);
   const [items, setItems] = useState(itemsIsMini ? numbersMini : numbers);
+  const [iconsIsMini, setIconsIsMini] = useState(true);
+  const [importIcons, setImportIcons] = useState(
+    iconsIsMini ? iconsMini : icons
+  );
   const [prev, setPrev] = useState(-1);
   const [hidden, setHidden] = useState(false);
   const [numberIsTrue, setNumberIsTrue] = useState(true);
 
   const check = (current) => {
-    if (items[current].id === items[prev].id) {
+    if (items[current].id == items[prev].id) {
       stopGamePlaysHandler();
       stopGameTimeNumber4x4();
       items[current].stat = "correct";
@@ -50,37 +54,73 @@ function Game({
     }
   };
 
+  const checkIcons = (current) => {
+    if (importIcons[current].id == importIcons[prev].id) {
+      numberOfMoveHandler();
+      importIcons[current].stat = "correct-icon";
+      importIcons[prev].stat = "correct-icon";
+      setItems([...importIcons]);
+      setPrev(-1);
+    } else {
+      numberOfMoveHandler();
+      importIcons[current].stat = "wrong-icon";
+      importIcons[prev].stat = "wrong-icon";
+      setItems([...importIcons]);
+      setHidden(true);
+      setTimeout(() => {
+        importIcons[current].stat = "";
+        importIcons[prev].stat = "";
+        setItems([...importIcons]);
+        setPrev(-1);
+        setHidden(false);
+      }, 1000);
+    }
+  };
+
+  const handleClickIcons = (id) => {
+    if (prev === -1) {
+      importIcons[id].stat = "active-icon";
+      setPrev(id);
+    } else {
+      checkIcons(id);
+    }
+  };
+
   return (
     <div className="Game">
       {numberIsTrue ? (
         <div className="">
           <div className={itemsIsMini ? "components-mini" : "components"}>
-            {items.map((item, index) => (
-              <NumberButton
-                key={index}
-                number={item.content}
-                handleClick={() => handleClick(index)}
-                id={index}
-                stat={item.stat}
-                itemsIsMini={itemsIsMini}
-              />
-            ))}
+            {items.map((item, index) => {
+              return (
+                <NumberButton
+                  key={index}
+                  number={item.content}
+                  handleClick={handleClick}
+                  id={index}
+                  stat={item.stat}
+                  itemsIsMini={itemsIsMini}
+                />
+              );
+            })}
           </div>
           <div className={hidden ? "hidden" : ""}></div>
         </div>
       ) : (
         <div className="">
-          <div className={itemsIsMini ? "components-mini" : "components"}>
-            {items.map((item, index) => (
-              <IconsButton
-                key={index}
-                importIcons={item.content}
-                handleClickIcons={() => handleClick(index)}
-                id={index}
-                stat={item.stat}
-                itemsIsMini={itemsIsMini}
-              />
-            ))}
+          <div className={iconsIsMini ? "components-mini" : "components"}>
+            {importIcons.map((icon, index) => {
+              return (
+                <IconsButton
+                  key={index}
+                  importIcons={icon.content}
+                  handleClickIcons={handleClickIcons}
+                  id={index}
+                  stat={icon.stat}
+                  iconsIsMini={iconsIsMini}
+                />
+              );
+            })}
           </div>
           <div className={hidden ? "hidden" : ""}></div>
         </div>
