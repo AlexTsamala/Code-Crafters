07529db46b4a46a753a3b35d4c9a8a22f2d6edbc
salomechik <salome.chikvaidze.05@gmail.com>
2023-09-15 01:12:05 -0700

    activated result page's button

---
+++ src/components/GamePlay/FooterMultiPlayer/FooterMultiPlayer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./FooterMultiPlayer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="triangle-box">
+      <div className="yellow-triangle"></div>
+        <div className="footer-box-active">
+          <h1 className="footer-text"> P 1</h1>
+          <h1 className="result-pairs">0</h1>
+        </div>
+      </div>
+      <div className="triangle-box">
+      <div className="yellow-triangle"></div>
+        <div className="footer-box-active">
+          <h1 className="footer-text"> P 1</h1>
+          <h1 className="result-pairs">0</h1>
+        </div>
+      </div>
+      <div className="triangle-box">
+      <div className="yellow-triangle"></div>
+        <div className="footer-box-active">
+          <h1 className="footer-text"> P 1</h1>
+          <h1 className="result-pairs">0</h1>
+        </div>
+      </div>
+      <div className="triangle-box">
+      <div className="yellow-triangle"></div>
+        <div className="footer-box-active">
+          <h1 className="footer-text"> P 1</h1>
+          <h1 className="result-pairs">0</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
